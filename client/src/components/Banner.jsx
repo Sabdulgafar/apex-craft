57665e44bb07e46a98471ae0760549f21bb68b53
Sabdulgafar <salawu.itopa1998@gmail.com>
2025-02-03 04:45:35 +0100

    adding my first set of designs using bootstrap

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,20 @@
+const Banner = ()=>{
+    return(
+        <div className="container-fluid banner">
+            <div className="row">
+                <div className="col">
+                    <h1>Left</h1>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12 right">
+                    <h1>HERE AT <span className="apexcraft">APEXCRAFT</span></h1>
+                    <p>We focus on helping to build your dreams and stay at the top of your goals.<br /> Your dreams, your goals, your ambition is our priority. </p>
+
+                    <button className="btn btn-dark">Contact us</button>
+                    <button className="btn btn-outline-dark">Sign up</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
---
+++ client/src/components/BlogSection.jsx
@@ -0,0 +1,72 @@
+const BlogSection = ()=>{
+    return(
+        <div className="container blog-section">
+            <div className="row">
+                <div className="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Blog Title</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Blog Post</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="/" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Blog Title</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Blog Post</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="/" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Blog Title</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Blog Post</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="/" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Blog Title</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Blog Post</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="/" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Blog Title</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Blog Post</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="/" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Blog Title</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Blog Post</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="/" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BlogSection;
